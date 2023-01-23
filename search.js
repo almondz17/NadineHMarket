5c8a2b6611815f32c055c736454a546ab5723934
Ahmad Majd <80617758+almondz17@users.noreply.github.com>
2023-01-23 06:44:15 -0600

    Add files via upload

---
+++ search.js
@@ -0,0 +1,833 @@
+let products = {
+  data: [
+    {
+      productName: "Mezban Beef Samosa",
+      category: "Frozen",
+    },
+    {
+      productName: "Mezban Chicken Samosa",
+      category: "Frozen",
+    },
+    {
+      productName: "Mezban Beef Kabob",
+      category: "Frozen",
+    },
+    {
+      productName: "Mezban Chicken Kabob",
+      category: "Frozen",
+    },
+    {
+      productName: "Alsafa Beef Patties",
+      category: "Frozen",
+    },
+    {
+      productName: "Alsafa Chicken Patties",
+      category: "Frozen",
+    },
+    {
+      productName: "Alsafa Chicken Nuggets",
+      category: "Frozen",
+    },
+    {
+      productName: "Alsafa Chicken Tenders",
+      category: "Frozen",
+    },
+    {
+      productName: "Alsafa Gyro",
+      category: "Frozen",
+    },
+    {
+      productName: "Mezban Potato Samosa",
+      category: "Frozen",
+    },
+    {
+      productName: "Mezban Cocktail Samosa",
+      category: "Frozen",
+    },
+    {
+      productName: "Mezban Vegetable Samosa",
+      category: "Frozen",
+    },
+    {
+      productName: "Mezban Tandoori Naan",
+      category: "Frozen",
+    },
+    {
+      productName: "Mezban Garlic Naan",
+      category: "Frozen",
+    },
+    {
+      productName: "Mezban Paratha",
+      category: "Frozen",
+    },
+    {
+      productName: "Gryo Bread",
+      category: "Frozen",
+    },
+    {
+      productName: "Kontos Bread",
+      category: "Frozen",
+    },
+    {
+      productName: "Kunafe Dough",
+      category: "Frozen",
+    },
+    {
+      productName: "Kunafe Cheese",
+      category: "Frozen",
+    },
+    {
+      productName: "Puff Pastry",
+    },
+    {
+      productName: "Kunafe",
+      category: "Frozen",
+    },
+    {
+      productName: "Okra 1",
+      category: "Frozen",
+    },
+    {
+      productName: "Okra 0",
+      category: "Frozen",
+    },
+    {
+      productName: "Molokhia",
+      category: "Frozen",
+    },
+    {
+      productName: "Mix vegetables",
+      category: "Frozen",
+    },
+    {
+      productName: "Gopi Yogurt 2lb",
+      category: "Dairy",
+    },
+    {
+      productName: "Gopi Yogurt 4lb",
+      category: "Dairy",
+    },
+    {
+      productName: "Karoun Yogurt 2lb",
+      category: "Dairy",
+    },
+    {
+      productName: "Karoun Yogurt 4lb",
+      category: "Dairy",
+    },
+    {
+      productName: "Karoun Yogurt Milk plain 1p",
+      category: "Dairy",
+    },
+    {
+      productName: "Karoun Yogrut Milk Mint 1p",
+      category: "Dairy",
+    },
+    {
+      productName: "Karoun Yogurt Milk plain 1/2g",
+      category: "Dairy",
+    },
+    {
+      productName: "Karoun Yogurt Milk Mint 1/2g",
+      category: "Dairy",
+    },
+    {
+      productName: "Karoun Yogurt Milk plain 1g",
+      category: "Dairy",
+    },
+    {
+      productName: "Karoun Yogurt Milk Mint 1g",
+      category: "Dairy",
+    },
+    {
+      productName: "White Chese",
+      category: "Dairy",
+    },
+    {
+      productName: "Gopi Lassi Mango",
+      category: "Dairy",
+    },
+    {
+      productName: "Gopi Lassi Rose",
+      category: "Dairy",
+    },
+    {
+      productName: "Feta Chese",
+      category: "Dairy",
+    },
+    {
+      productName: "Kechval Chese",
+      category: "Dairy",
+    },
+    {
+      productName: "Lebneh",
+      category: "Dairy",
+    },
+    {
+      productName: "Puck Cream",
+      category: "Dairy",
+    },
+    {
+      productName: "Turkish Lebneh",
+      category: "Dairy",
+    },
+    {
+      productName: "Ziyad Mango Juice",
+      category: "Beverages",
+    },
+    {
+      productName: "Barbican Pinapple",
+      category: "Beverages",
+    },
+    {
+      productName: "Barbican Apple",
+      category: "Beverages",
+    },
+    {
+      productName: "Barbican Malt",
+      category: "Beverages",
+    },
+    {
+      productName: "Jus Coconut Juice",
+      category: "Beverages",
+    },
+    {
+      productName: "Jus Pommegranate Juice",
+      category: "Beverages",
+    },
+    {
+      productName: "Fresher Orange",
+      category: "Beverages",
+    },
+    {
+      productName: "Fresher Strawberry",
+      category: "Beverages",
+    },
+    {
+      productName: "Fresher Pina Colada",
+      category: "Beverages",
+    },
+    {
+      productName: "Ziyad Pommegranate Juice",
+      category: "Beverages",
+    },
+    {
+      productName: "Vimto",
+      category: "Beverages",
+    },
+    {
+      productName: "Schweps Pinapple",
+      category: "Beverages",
+    },
+    {
+      productName: "Schweps Tangerine",
+      category: "Beverages",
+    },
+    {
+      productName: "Jus Orange Juice",
+      category: "Beverages",
+    },
+    {
+      productName: "Turkish Coke",
+      category: "Beverages",
+    },
+    {
+      productName: "Turkish Pepsi",
+      category: "Beverages",
+    },
+    {
+      productName: "Turkish Sprite",
+      category: "Beverages",
+    },
+    {
+      productName: "Mineral Water",
+      category: "Beverages",
+    },
+    {
+      productName: "Zam Zam Water",
+      category: "Beverages",
+    },
+    {
+      productName: "Laziza Apple",
+      category: "Beverages",
+    },
+    {
+      productName: "Laziza Strawberry",
+      category: "Beverages",
+    },
+    {
+      productName: "Royal Basmati 20LB",
+      category: "Rice",
+    },
+    {
+      productName: "Royal Basmati 10LB",
+      category: "Rice",
+    },
+    {
+      productName: "Chef Secret 10LB",
+      category: "Rice",
+    },
+    {
+      productName: "Mahmood Rice",
+      category: "Rice",
+    },
+    {
+      productName: "Khor Rice",
+      category: "Rice",
+    },
+    {
+      productName: "Zebra Rice",
+      category: "Rice",
+    },
+    {
+      productName: "Falak Rice",
+      category: "Rice",
+    },
+    {
+      productName: "Royal Basmati Brown 10LB",
+      category: "Rice",
+    },
+    {
+      productName: "Ocean Pearl 10LB",
+      category: "Rice",
+    },
+    {
+      productName: "Zebra Sella Rice",
+      category: "Rice",
+    },
+    {
+      productName: "Zebra Brown Rice",
+      category: "Rice",
+    },
+    {
+      productName: "Masoor Dal 4LB",
+      category: "24",
+    },
+    {
+      productName: "Masoor Dal 2LB",
+      category: "Daals",
+    },
+    {
+      productName: "Chana 2LB",
+      category: "Daals",
+    },
+    {
+      productName: "Chana 4LB",
+      category: "Daals",
+    },
+    {
+      productName: "Red Lentils 2LB",
+      category: "Daals",
+    },
+    {
+      productName: "Red Lentils 4LB",
+      category: "Daals",
+    },
+    {
+      productName: "Brown Lentils 2LB",
+      category: "Daals",
+    },
+    {
+      productName: "Brown Lentils 4LB",
+      category: "Daals",
+    },
+    {
+      productName: "Garbanzo 2LB",
+      category: "Daals",
+    },
+    {
+      productName: "Garbanzo 4LB",
+      category: "Daals",
+    },
+    {
+      productName: "Large Green Lentils 2LB",
+      category: "Daals",
+    },
+    {
+      productName: "Large Green Lentils 4LB",
+      category: "Daals",
+    },
+    {
+      productName: "Small Green Lentils 2LB",
+      category: "Daals",
+    },
+    {
+      productName: "Small Green Lentils 4LB",
+      category: "Daals",
+    },
+    {
+      productName: "Ziyad Pickles",
+      category: "Jars",
+    },
+    {
+      productName: "Ziyad Turnips",
+      category: "Jars",
+    },
+    {
+      productName: "Ziyad Makdous",
+      category: "Jars",
+    },
+    {
+      productName: "Ziyad Mild Giardiniera Small",
+      category: "Jars",
+    },
+    {
+      productName: "Ziyad Mild Giardiniera Large",
+      category: "Jars",
+    },
+    {
+      productName: "Ziyad Spicy Giardiniera Large",
+      category: "Jars",
+    },
+    {
+      productName: "Ziiyad Spicy Giardiniera Small",
+      category: "Jars",
+    },
+    {
+      productName: "Casablanca Pickled Lemon",
+      category: "Jars",
+    },
+    {
+      productName: "Green Olives",
+      category: "Jars",
+    },
+    {
+      productName: "Pited Green Olives",
+      category: "Jars",
+    },
+    {
+      productName: "Green Olives With Lemon",
+      category: "Jars",
+    },
+    {
+      productName: "Green Olives With Thyme",
+      category: "Jars",
+    },
+    {
+      productName: "Kalamata Olives",
+      category: "Jars",
+    },
+    {
+      productName: "Dried Kalamata Olives",
+      category: "Jars",
+    },
+    {
+      productName: "Black Olives",
+      category: "Jars",
+    },
+    {
+      productName: "Olive Oil",
+      category: "Jars",
+    },
+    {
+      productName: "Sesame Oil",
+      category: "Jars",
+    },
+    {
+      productName: "Peanut Oil",
+      category: "Jars",
+    },
+    {
+      productName: "Tomato Paste",
+      category: "Baking",
+    },
+    {
+      productName: "Baking Powder",
+      category: "Baking",
+    },
+    {
+      productName: "Yeast",
+      category: "Baking",
+    },
+    {
+      productName: "Vanilla Powder",
+      category: "Baking",
+    },
+    {
+      productName: "Wheat Flour",
+      category: "Baking",
+    },
+    {
+      productName: "All Purpose Flour",
+      category: "Baking",
+    },
+    {
+      productName: "Corn Flour",
+      category: "Baking",
+    },
+    {
+      productName: "Atta",
+      category: "Baking",
+    },
+    {
+      productName: "Ziyad Ghee 16OZ",
+      category: "Baking",
+    },
+    {
+      productName: "Ziyad Ghee 32OZ",
+      category: "Baking",
+    },
+    {
+      productName: "Kunafa Food Color",
+      category: "Baking",
+    },
+    {
+      productName: "Ziyad Ammonia Powder",
+      category: "Baking",
+    },
+    {
+      productName: "Ziyad Vanilla",
+      category: "Baking",
+    },
+    {
+      productName: "Ziyad Kunafa Food Color",
+      category: "Baking",
+    },
+    {
+      productName: "Cocoa Powder",
+      category: "Baking",
+    },
+    {
+      productName: "Salt",
+      category: "Spices",
+    },
+    {
+      productName: "Black Pepper Whole",
+      category: "Spices",
+    },
+    {
+      productName: "Black Pepper Ground",
+      category: "Spices",
+    },
+    {
+      productName: "Paprika",
+      category: "Spices",
+    },
+    {
+      productName: "Coriander Whole",
+      category: "Spices",
+    },
+    {
+      productName: "Coriander Ground",
+      category: "Spices",
+    },
+    {
+      productName: "Curry Mild",
+      category: "Spices",
+    },
+    {
+      productName: "Curry Hot",
+      category: "Spices",
+    },
+    {
+      productName: "Makloube Spices",
+      category: "Spices",
+    },
+    {
+      productName: "Tandoori Spice",
+      category: "Spices",
+    },
+    {
+      productName: "Meat Spices",
+      category: "Spices",
+    },
+    {
+      productName: "Shan Butter Chicken",
+      category: "Spices",
+    },
+    {
+      productName: "Shan Bombay Bryani",
+      category: "Spices",
+    },
+    {
+      productName: "Shan Chicken Tika",
+      category: "Spices",
+    },
+    {
+      productName: "Shan Chana Chat",
+      category: "Spices",
+    },
+    {
+      productName: "Shan Chicken Shawarma",
+      category: "Spices",
+    },
+    {
+      productName: "Shan Sindhi Bryani",
+      category: "Spices",
+    },
+    {
+      productName: "Shan Mutton Bryani",
+      category: "Spices",
+    },
+    {
+      productName: "Garlic Powder",
+      category: "Spices",
+    },
+    {
+      productName: "Ginger Powder",
+      category: "Spices",
+    },
+    {
+      productName: "Kebeh Spice",
+      category: "Spices",
+    },
+    {
+      productName: "Elbow",
+      category: "Pasta",
+    },
+    {
+      productName: "Spaghetti",
+      category: "Pasta",
+    },
+    {
+      productName: "Vermicelli",
+      category: "Pasta",
+    },
+    {
+      productName: "Fusilli",
+      category: "Pasta",
+    },
+    {
+      productName: "Macaroni",
+      category: "Pasta",
+    },
+    {
+      productName: "Penne",
+      category: "Pasta",
+    },
+    {
+      productName: "Orzo",
+      category: "Pasta",
+    },
+    {
+      productName: "Farfalle",
+      category: "Pasta",
+    },
+    {
+      productName: "Lasagna",
+      category: "Pasta",
+    },
+    {
+      productName: "Sweet and Spicy Chips",
+      category: "Snacks",
+    },
+    {
+      productName: "Kabab Chips",
+      category: "Snacks",
+    },
+    {
+      productName: "Ketchup Chips",
+      category: "Snacks",
+    },
+    {
+      productName: "Lemon Chips",
+      category: "Snacks",
+    },
+    {
+      productName: "Milka Oreo Chocolate",
+      category: "Snacks",
+    },
+    {
+      productName: "Milka Plain Chocolate",
+      category: "Snacks",
+    },
+    {
+      productName: "Milka Caramel Chocolate",
+      category: "Snacks",
+    },
+    {
+      productName: "Milka Lupo Chocolate",
+      category: "Snacks",
+    },
+    {
+      productName: "Kinder Chocolate",
+      category: "Snacks",
+    },
+    {
+      productName: "Bounty Chocolate",
+      category: "Snacks",
+    },
+    {
+      productName: "Paprika Chips",
+      category: "Snacks",
+    },
+    {
+      productName: "Cheese Chips",
+      category: "Snacks",
+    },
+    {
+      productName: "Cheese Balls",
+      category: "Snacks",
+    },
+    {
+      productName: "Milka Cookies",
+      category: "Snacks",
+    },
+    {
+      productName: "Ali Baba Wafers Plain",
+      category: "Snacks",
+    },
+    {
+      productName: "Ali Baba Wafers Coconut",
+      category: "Snacks",
+    },
+    {
+      productName: "Ali Baba Wafers Dark Chocolate",
+      category: "Snacks",
+    },
+    {
+      productName: "Roasted Peanuts",
+      category: "Snacks",
+    },
+    {
+      productName: "Spicy Roasted Peanuts",
+      category: "Snacks",
+    },
+    {
+      productName: "Two For You Wafers",
+      category: "Snacks",
+    },
+    {
+      productName: "Ali Baba Wafers White Chocolate",
+      category: "Snacks",
+    },
+    {
+      productName: "CG Palestinian Recipe",
+      category: "Cans",
+    },
+    {
+      productName: "CG Lebanese Recipe",
+      category: "Cans",
+    },
+    {
+      productName: "CG Saudi Recipe",
+      category: "Cans",
+    },
+    {
+      productName: "CG Sudani Recipe",
+      category: "Cans",
+    },
+    {
+      productName: "CG Fool Mashed",
+      category: "Cans",
+    },
+    {
+      productName: "Ziyad Hummus Spicy",
+      category: "Cans",
+    },
+    {
+      productName: "Ziyad Hummus Plain",
+      category: "Cans",
+    },
+    {
+      productName: "Talia Hummus",
+      category: "Cans",
+    },
+    {
+      productName: "talia Fava Beans",
+      category: "Cans",
+    },
+    {
+      productName: "Ziyad Pinto Beans",
+      category: "Cans",
+    },
+    {
+      productName: "Ziyad Red Kidney Beans",
+      category: "Cans",
+    },
+    {
+      productName: "Tazah Saudi Recipe",
+      category: "Cans",
+    },
+    {
+      productName: "Tazah Peeled Secret Recipe",
+      category: "Cans",
+    },
+    {
+      productName: "Tazah Secret Recipe",
+      category: "Cans",
+    },
+    {
+      productName: "Ziyad Chinese Fava Beans",
+      category: "Cans",
+    },
+    {
+      productName: "Ziyad Large Fava Beans",
+      category: "Cans",
+    },
+  ],
+};
+
+for (let i of products.data) {
+  //Create Card
+  let card = document.createElement("div");
+  //Card should have category and should stay hidden initially
+  card.classList.add("card", i.category, "hide");
+  //image div
+  
+  //container
+  let container = document.createElement("div");
+  container.classList.add("container");
+  //product name
+  let name = document.createElement("h5");
+  name.classList.add("product-name");
+  name.innerText = i.productName.toUpperCase();
+  container.appendChild(name);
+  //price
+
+  card.appendChild(container);
+  document.getElementById("products").appendChild(card);
+}
+
+//parameter passed from button (Parameter same as category)
+function filterProduct(value) {
+  //Button class code
+  let buttons = document.querySelectorAll(".button-value");
+  buttons.forEach((button) => {
+    //check if value equals innerText
+    if (value.toUpperCase() == button.innerText.toUpperCase()) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
+
+  //select all cards
+  let elements = document.querySelectorAll(".card");
+  //loop through all cards
+  elements.forEach((element) => {
+    //display all cards on 'all' button click
+    if (value == "") {
+      element.classList.remove("hide");
+    } else {
+      //Check if element contains category class
+      if (element.classList.contains(value)) {
+        //display element based on category
+        element.classList.remove("hide");
+      } else {
+        //hide other elements
+        element.classList.add("hide");
+      }
+    }
+  });
+}
+
+//Search button click
+document.getElementById("search").addEventListener("click", () => {
+  //initializations
+  let searchInput = document.getElementById("search-input").value;
+  let elements = document.querySelectorAll(".product-name");
+  let cards = document.querySelectorAll(".card");
+
+  //loop through all elements
+  elements.forEach((element, index) => {
+    //check if text includes the search value
+    if (element.innerText.includes(searchInput.toUpperCase())) {
+      //display matching card
+      cards[index].classList.remove("hide");
+    } else {
+      //hide others
+      cards[index].classList.add("hide");
+    }
+  });
+});
